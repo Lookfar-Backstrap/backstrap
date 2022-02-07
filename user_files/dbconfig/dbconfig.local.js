049b996beb261c401e1b69bdd049983f73767913
Jonathan Biguenet <jonnyb@lookfar.com>
2022-02-07 11:15:39 -0600

    - sanitizing for release

--- user_files/dbconfig/dbconfig.local.js
+++ user_files/dbconfig/dbconfig.local.js
@@ -1,8 +1,8 @@
 module.exports = {
   db: {
-    user: process.env.DB_USER || 'backstrap_user',
-    name: process.env.DB_NAME || 'backstrap',
-    pass: process.env.DB_PASS || 'bs4u',
+    user: process.env.DB_USER || '[YOUR DB USER HERE]',
+    name: process.env.DB_NAME || '[YOUR DB NAME HERE]',
+    pass: process.env.DB_PASS || '[YOUR DB PASSWORD HERE]',
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || '5432'
   }
